4cea44738d44ddf32175a668fa5227146275a8d8
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-10-16 23:23:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./components/layout";
+import { Home } from "./pages/home";
+import { Error } from "./pages/error";
+
+const router = createBrowserRouter([
+  {
+    element:<Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>,
+      },
+      {
+        path:"*",
+        element:<Error/>
+      }
+    ]
+  }
+])
+
+export {router};
---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return (
+        <div>
+            <header>
+                <h1> COMPONENTE HEADER</h1>
+            </header>
+        </div>
+    )
+}
